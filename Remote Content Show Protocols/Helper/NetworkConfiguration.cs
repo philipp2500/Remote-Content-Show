cbcfbdf0874c933f78c020c5a1982e88066b1f3f
philipp2500 <96303@fhwn.ac.at>
2016-01-20 15:38:20 +0100

    Port added

---
+++ Remote Content Show Protocols/Helper/NetworkConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remote_Content_Show_Protocol
+{
+    public static class NetworkConfiguration
+    {
+        public const int Port = 1049;
+    }
+}
