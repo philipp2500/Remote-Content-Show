19c37bbd3f50a9914582e2c754fa1d838fba399d
mhofer4991 <mhofer4991@gmail.com>
2016-01-20 14:48:56 +0100

    Added Sockethandler, which obviously handles the sockets.

---
+++ DisplayClient/Network/SocketHandler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Sockets;
+using Remote_Content_Show_Protocol;
+using Windows.Storage.Streams;
+using Remote_Content_Show_Container;
+
+namespace DisplayClient.Network
+{
+    public class SocketHandler
+    {
+        private StreamSocket socket;
+
+        private bool running;
+
+        public SocketHandler(StreamSocket socket)
+        {
+            this.socket = socket;
+        }
+
+        public delegate void MessageBytesReceived(MessageCode code, byte[] bytes);
+
+        public event MessageBytesReceived OnMessageBytesReceived;
+
+        public void Start()
+        {
+            this.running = true;
+
+            Task.Factory.StartNew(() =>
+            {
+                this.HandleSocket();
+            });
+        }
+
+        private async void HandleSocket()
+        {
+            DataReader reader = new DataReader(this.socket.InputStream);
+            DataWriter writer = new DataWriter(this.socket.OutputStream);
+
+            uint headerSize = Convert.ToUInt32(Remote_Content_Show_Header.HeaderLength);
+
+            while (this.running)
+            {
+                if (await reader.LoadAsync(headerSize) == headerSize)
+                {
+                    byte[] headerBytes = new byte[headerSize];
+
+                    reader.ReadBytes(headerBytes);
+
+                    Remote_Content_Show_Header header = Remote_Content_Show_Header.FromByte(headerBytes);
+
+                    uint contentSize = Convert.ToUInt32(header.Length);
+
+                    if (await reader.LoadAsync(contentSize) == contentSize)
+                    {
+                        byte[] contentBytes = new byte[contentSize];
+
+                        reader.ReadBytes(contentBytes);
+
+                        if (this.OnMessageBytesReceived != null)
+                        {
+                            this.OnMessageBytesReceived(header.Code, contentBytes);
+                        }
+                    }
+                }
+                else
+                {
+                    await Task.Delay(1000);
+                }
+            }
+        }
+    }
+}
