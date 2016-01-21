d321f903f5ead3baecd052891d3c8bea9ce3f965
philipp2500 <96303@fhwn.ac.at>
2016-01-21 10:38:37 +0100

    Lib

--- ConfigManager/NetworkManager.cs
+++ ConfigManager/NetworkManager.cs
@@ -3,11 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using Remote_Content_Show_Protocol;
+using Remote_Content_Show_Container;
 
 namespace ConfigManager
 {
     public class NetworkManager
     {
+        public event EventHandler<MessageRecivedEventHandler> OnMessageReceived;
+        public event EventHandler<ErrorEventHandler> OnError;
 
+        private List<NetworkConnection> connections = new List<NetworkConnection>();
+
+        public NetworkConnection ConnectTo(IPAddress ip)
+        {
+            NetworkConnection netCon = new NetworkConnection(ip);
+            netCon.OnError += NetCon_OnError;
+            netCon.OnMessageReceived += NetCon_OnMessageReceived;
+            netCon.Connect();
+            this.connections.Add(netCon);
+
+            return netCon;
+        }
+
+        public void Write(NetworkConnection netCon, byte[] messageData, MessageCode code)
+        {
+            netCon.Write(messageData, code);
+        }
+
+        public void CloseAllConnection()
+        {
+            foreach(NetworkConnection c in this.connections)
+            {
+                c.Close();
+            }
+        }
+
+        protected void FireOnMessageReceived(MessageRecivedEventHandler e)
+        {
+            if (this.OnMessageReceived != null)
+            {
+                OnMessageReceived(this, e);
+            }
+        }
+
+        protected void FireOnError(ErrorEventHandler e)
+        {
+            if (this.OnError != null)
+            {
+                OnError(this, e);
+            }
+        }
+
+        private void NetCon_OnMessageReceived(object sender, MessageRecivedEventHandler e)
+        {
+            this.FireOnMessageReceived(e);
+        }
+
+        private void NetCon_OnError(object sender, ErrorEventHandler e)
+        {
+            this.connections.Remove((NetworkConnection)sender);
+            this.FireOnError(e);
+        }
     }
 }
--- ConfigManager/NewJob.xaml.cs
+++ ConfigManager/NewJob.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Net;
+using Remote_Content_Show_Protocol;
+using Remote_Content_Show_Container;
 
 namespace ConfigManager
 {
@@ -19,10 +22,48 @@ namespace ConfigManager
     /// </summary>
     public partial class NewJob : Window
     {
+        private NetworkManager netmanager = new NetworkManager();
+
         public NewJob()
         {
             InitializeComponent();
+            this.netmanager.OnError += Netmanager_OnError;
+            this.netmanager.OnMessageReceived += Netmanager_OnMessageReceived;
+        }
 
+        private void Netmanager_OnMessageReceived(object sender, MessageRecivedEventHandler e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Netmanager_OnError(object sender, ErrorEventHandler e)
+        {
+            MessageBox.Show(e.Text, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void AddAgent_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(this.AgentConnection.Text))
+            {
+                try
+                {
+                    IPAddress[] ips = Dns.GetHostAddresses(this.AgentConnection.Text);
+                    if (ips.Length == 0)
+                    {
+                        MessageBox.Show("IP/Name konnte nicht aufgelöst werden!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    else
+                    {
+                        NetworkConnection netC = this.netmanager.ConnectTo(ips[0]);
+                        byte[] messageData = Remote_Content_Show_MessageGenerator.GetMessageAsByte(new RCS_Process_List_Request());
+                        netC.Write(messageData, MessageCode.MC_Process_List_Request);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("IP/Name konnte nicht aufgelöst werden!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
