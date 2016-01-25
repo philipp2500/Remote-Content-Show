0b46ecf9681e3215a95530c796843e5fbea77c07
philipp2500 <96303@fhwn.ac.at>
2016-01-25 17:14:58 +0100

    Start Layouter

--- ConfigManager/Properties/Resources.Designer.cs
+++ ConfigManager/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace ConfigManager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap layoutdisigner {
+            get {
+                object obj = ResourceManager.GetObject("layoutdisigner", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap loadFromFile {
             get {
                 object obj = ResourceManager.GetObject("loadFromFile", resourceCulture);
