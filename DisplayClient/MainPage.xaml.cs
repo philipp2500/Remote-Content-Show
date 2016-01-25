c46107e5bb1a1e24d40f6eec55cf0b47048225ff
mhofer4991 <mhofer4991@gmail.com>
2016-01-25 09:58:52 +0100

    the life of shit pi

--- DisplayClient/MainPage.xaml.cs
+++ DisplayClient/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
