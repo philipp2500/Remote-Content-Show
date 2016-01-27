8f01a00f0d71c5aafb0f39615f602a551898716e
Philipp Leister <philipp.leister@outlook.com>
2016-01-27 21:18:04 +0100

    ITServer changed

--- ITServicesDataServer/Client.cs
+++ ITServicesDataServer/Client.cs
@@ -20,6 +20,7 @@ namespace ITServicesDataServer
         private SqlConnection connetion = new SqlConnection("Data Source=datafhdbF1;Initial Catalog=FHDB;Persist Security Info=True;User ID=www_stuplan;Password=Export_stuplan");
 
         //-http://code.tutsplus.com/tutorials/http-headers-for-dummies--net-8039
+        //-http://webmasterjuice.com/how-to-activate-built-in-web-server-windows
 
         public event EventHandler<RequestHandler> OnRequest;
 
@@ -41,54 +42,57 @@ namespace ITServicesDataServer
 
         private void Work()
         {
-            NetworkStream stream = this.client.GetStream();
-            StreamReader reader = new StreamReader(stream);
-            string line = reader.ReadLine();
-            if (!string.IsNullOrWhiteSpace(line))
+            try
             {
-                string[] input = line.Split(' ');
-                if (input.Length == 3)
+                NetworkStream stream = this.client.GetStream();
+                StreamReader reader = new StreamReader(stream);
+                string line = reader.ReadLine();
+                if (!string.IsNullOrWhiteSpace(line))
                 {
-                    if (input[0] == "GET" && input[1].StartsWith("/room="))
+                    string[] input = line.Split(' ');
+                    if (input.Length == 3)
                     {
-                        this.FireOnRequest(line, IPAddress.Parse(((IPEndPoint)this.client.Client.RemoteEndPoint).Address.ToString()));
-                        string paramether = input[1].Remove(0, 6);
-                        //SqlCommand command = new SqlCommand("SELECT TOP(3) * FROM v_stuplan WHERE SAAL = '" + paramether + "' ORDER BY DATUM, VON", connetion);
-                        SqlCommand command = new SqlCommand("SELECT TOP(3) * FROM v_stuplan WHERE SAAL = '" + paramether + "' ORDER BY DATUM, VON", connetion);
-                        this.connetion.Open();
-                        SqlDataReader sqlResult = command.ExecuteReader();
-                        List<ReslutItem> erg = new List<ReslutItem>();
-                        while(sqlResult.Read())
+                        if (input[0] == "GET" && input[1].StartsWith("/room="))
                         {
-                            ReslutItem ri = new ReslutItem();
-                            ri.Datum = sqlResult.GetDateTime(0);
-                            ri.Von = sqlResult.GetTimeSpan(1);
-                            ri.Bis = sqlResult.GetTimeSpan(2);
-                            ri.Saal = sqlResult.GetString(3);
-                            ri.Bezeichnung = sqlResult.GetString(4);
-                            ri.LVArt = sqlResult.GetString(5);
-                            ri.LVBezeichnung = sqlResult.GetString(6);
-                            erg.Add(ri);
+                            this.FireOnRequest(line, IPAddress.Parse(((IPEndPoint)this.client.Client.RemoteEndPoint).Address.ToString()));
+                            string paramether = input[1].Remove(0, 6);
+                            SqlCommand command = new SqlCommand("SELECT TOP(3) * FROM v_stuplan WHERE SAAL = '" + paramether + "' ORDER BY DATUM, VON", connetion);
+                            this.connetion.Open();
+                            SqlDataReader sqlResult = command.ExecuteReader();
+                            List<ReslutItem> erg = new List<ReslutItem>();
+                            while (sqlResult.Read())
+                            {
+                                ReslutItem ri = new ReslutItem();
+                                ri.Datum = sqlResult.GetDateTime(0);
+                                ri.Von = sqlResult.GetTimeSpan(1);
+                                ri.Bis = sqlResult.GetTimeSpan(2);
+                                ri.Saal = sqlResult.GetString(3);
+                                ri.Bezeichnung = sqlResult.GetString(4);
+                                ri.LVArt = sqlResult.GetString(5);
+                                ri.LVBezeichnung = sqlResult.GetString(6);
+                                erg.Add(ri);
+                            }
+
+                            List<byte> response = new List<byte>();
+                            response.AddRange(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n"));
+                            response.AddRange(Encoding.UTF8.GetBytes("Content-Type: application/json; charset=UTF-8\r\n\r\n"));
+                            response.AddRange(Remote_Content_Show_MessageGenerator.GetMessageAsByte(erg));
+                            byte[] data = response.ToArray();
+                            stream.Write(data, 0, data.Length);
+
+                            this.connetion.Close();
                         }
-
-                        List<byte> response = new List<byte>();
-                        response.AddRange(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n"));
-                        response.AddRange(Encoding.UTF8.GetBytes("Content-Type: application/json; charset=UTF-8\r\n\r\n"));
-                        response.AddRange(Remote_Content_Show_MessageGenerator.GetMessageAsByte(erg));
-                        byte[] data = response.ToArray();
-                        stream.Write(data, 0, data.Length);
-                        //response.Content = Remote_Content_Show_MessageGenerator.toJason(erg);
-
-
-
-                        this.connetion.Close();
                     }
                 }
-            }
 
-            reader.Close();
-            stream.Close();
-            this.client.Close();
+                reader.Close();
+                stream.Close();
+                this.client.Close();
+            }
+            catch
+            {
+
+            }
         }
     }
 }
